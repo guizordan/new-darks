d341297d5702075f5ff25c2e0ac5b892234a25a1
Guilherme Zordan <guilhermezordan@outlook.com>
2017-10-01 22:17:20 -0300

    quase pronto

--- index.php
+++ index.php
@@ -1,42 +1,42 @@
 <!DOCTYPE html>
 <html lang="pt-br" id="page-top">
   <head>
-      <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
-      <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
-      <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
-      <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
-      <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
-      <link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
-      <link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
-      <link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
-      <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
-      <link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
-      <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
-      <link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
-      <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
-      <link rel="manifest" href="favicon/manifest.json">
-      <meta name="msapplication-TileColor" content="#ffffff">
-      <meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
-      <meta name="theme-color" content="#5cc6d0">
+    <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#5cc6d0">
 
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <meta name="description" content="A New Dark's Informática é uma loja de equipamentos de informática, móveis e cadeiras de escritório, sistemas gerenciais e instrumentos musicais. Especializada em assistência técnica e certificação digital, fica situada em Casca-RS">
-      <meta name="keywords" content="informática, darks, dark's, new, equipamentos, assistência, técnica, certificacao, digital, instrumentos, musicais, loja, casca, rio, grande, do, sul, rs">
-      <meta name="author" content="">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="A New Dark's Informática é uma loja de equipamentos de informática, móveis e cadeiras de escritório, sistemas gerenciais e instrumentos musicais. Especializada em assistência técnica e certificação digital, fica situada em Casca-RS">
+    <meta name="keywords" content="informática, darks, dark's, new, equipamentos, assistência, técnica, certificacao, digital, instrumentos, musicais, loja, casca, rio, grande, do, sul, rs">
+    <meta name="author" content="">
 
-      <title>New Dark's Informática - Sempre o melhor pra você</title>
+    <title>New Dark's Informática - Sempre o melhor pra você</title>
 
-      <!-- Bootstrap core CSS -->
-      <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-      <!-- Custom fonts for this template -->
-      <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-      <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-      <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
 
-      <!-- Custom styles for this template -->
-      <link href="css/freelancer.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/freelancer.min.css" rel="stylesheet">
 
   </head>
 
@@ -65,11 +65,10 @@
     <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Contact Form JavaScript -->
-    <!-- <script src="js/jqBootstrapValidation.js"></script>
-    <script src="js/contact_me.js"></script> -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
 
     <!-- Custom scripts for this template -->
-    <!-- <script src="js/freelancer.min.js"></script> -->
+    <script src="js/freelancer.min.js"></script>
   </body>
-
 </html>
