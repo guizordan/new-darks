b36d67097c01854dea09e9c52575e7c785637105
Gui Zordan <guilhermezordan@outlook.com>
2025-03-27 18:11:25 -0300

    package json updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,109 +1,124 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
-var header = require('gulp-header');
-var cleanCSS = require('gulp-clean-css');
-var rename = require("gulp-rename");
-var uglify = require('gulp-uglify');
-var filter = require('gulp-filter');
-var pkg = require('./package.json');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const browserSync = require("browser-sync").create();
+const header = require("gulp-header");
+const cleanCSS = require("gulp-clean-css");
+const rename = require("gulp-rename");
+const uglify = require("gulp-uglify");
+const pkg = require("./package.json");
 
-// Set the banner content
-var banner = ['/*!\n',
-  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
-  ' */\n',
-  ''
-].join('');
+// Banner
+const banner = `/*!
+ * Start Bootstrap - ${pkg.title} v${pkg.version} (${pkg.homepage})
+ * Copyright 2013-${new Date().getFullYear()} ${pkg.author}
+ * Licensed under ${pkg.license} (https://github.com/BlackrockDigital/${
+  pkg.name
+}/blob/master/LICENSE)
+ */
+`;
 
-// Compiles SCSS files from /scss into /css
-gulp.task('sass', function() {
-  return gulp.src('scss/freelancer.scss')
-    .pipe(sass())
-    .pipe(header(banner, { pkg: pkg }))
-    .pipe(gulp.dest('css'))
-    .pipe(browserSync.reload({
-        stream: true
-    }))
-});
+// Compila SCSS para CSS
+function sassTask() {
+  return gulp
+    .src("scss/freelancer.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(header(banner, { pkg }))
+    .pipe(gulp.dest("css"))
+    .pipe(browserSync.stream());
+}
 
-// Minify compiled CSS
-gulp.task('minify-css', ['sass'], function() {
-  return gulp.src('css/freelancer.css')
-    .pipe(cleanCSS({ compatibility: 'ie8' }))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('css'))
-    .pipe(browserSync.reload({
-        stream: true
-    }))
-});
+// Minifica o CSS
+function minifyCss() {
+  return gulp
+    .src("css/freelancer.css")
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("css"))
+    .pipe(browserSync.stream());
+}
 
-// Minify custom JS
-gulp.task('minify-js', function() {
-  return gulp.src('js/freelancer.js')
+// Minifica o JS
+function minifyJs() {
+  return gulp
+    .src("js/freelancer.js")
     .pipe(uglify())
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('js'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-});
+    .pipe(header(banner, { pkg }))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("js"))
+    .pipe(browserSync.stream());
+}
 
-// Copy vendor files from /node_modules into /vendor
-// NOTE: requires `npm install` before running!
-gulp.task('copy', function() {
-  gulp.src([
-      'node_modules/bootstrap/dist/**/*',
-      '!**/npm.js',
-      '!**/bootstrap-theme.*',
-      '!**/*.map'
-    ])
-    .pipe(gulp.dest('vendor/bootstrap'))
+// Copia arquivos para a pasta vendor
+function copyFiles() {
+  return Promise.all([
+    gulp
+      .src([
+        "node_modules/bootstrap/dist/**/*",
+        "!**/npm.js",
+        "!**/bootstrap-theme.*",
+        "!**/*.map",
+      ])
+      .pipe(gulp.dest("vendor/bootstrap")),
 
-  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-    .pipe(gulp.dest('vendor/jquery'))
+    gulp
+      .src([
+        "node_modules/jquery/dist/jquery.js",
+        "node_modules/jquery/dist/jquery.min.js",
+      ])
+      .pipe(gulp.dest("vendor/jquery")),
 
-  gulp.src(['node_modules/popper.js/dist/umd/popper.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-    .pipe(gulp.dest('vendor/popper'))
+    gulp
+      .src([
+        "node_modules/popper.js/dist/umd/popper.js",
+        "node_modules/popper.js/dist/umd/popper.min.js",
+      ])
+      .pipe(gulp.dest("vendor/popper")),
 
-  gulp.src(['node_modules/jquery.easing/*.js'])
-    .pipe(gulp.dest('vendor/jquery-easing'))
+    gulp
+      .src(["node_modules/jquery.easing/*.js"])
+      .pipe(gulp.dest("vendor/jquery-easing")),
 
-  gulp.src([
-      'node_modules/font-awesome/**',
-      '!node_modules/font-awesome/**/*.map',
-      '!node_modules/font-awesome/.npmignore',
-      '!node_modules/font-awesome/*.txt',
-      '!node_modules/font-awesome/*.md',
-      '!node_modules/font-awesome/*.json'
-    ])
-    .pipe(gulp.dest('vendor/font-awesome'))
-})
+    gulp
+      .src([
+        "node_modules/font-awesome/**",
+        "!node_modules/font-awesome/**/*.map",
+        "!node_modules/font-awesome/.npmignore",
+        "!node_modules/font-awesome/*.txt",
+        "!node_modules/font-awesome/*.md",
+        "!node_modules/font-awesome/*.json",
+      ])
+      .pipe(gulp.dest("vendor/font-awesome")),
+  ]);
+}
 
-// Default task
-gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy']);
-
-// Configure the browserSync task
-gulp.task('browserSync', function() {
+// Configura o BrowserSync
+function browserSyncTask(done) {
   browserSync.init({
-    server: {
-      baseDir: ''
-    },
-  })
-})
+    server: { baseDir: "" },
+  });
+  done();
+}
 
-// Dev task with browserSync
-gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() {
-  gulp.watch('scss/*.scss', ['sass']);
-  gulp.watch('css/*.css', ['minify-css']);
-  gulp.watch('js/*.js', ['minify-js']);
-  // Reloads the browser whenever HTML or JS files change
-  gulp.watch('*.html', browserSync.reload);
-  gulp.watch('js/**/*.js', browserSync.reload);
-});
+// Watch para mudanças
+function watchFiles() {
+  gulp.watch("scss/*.scss", sassTask);
+  gulp.watch("css/*.css", minifyCss);
+  gulp.watch("js/*.js", minifyJs);
+  gulp.watch("*.html").on("change", browserSync.reload);
+  gulp.watch("js/**/*.js").on("change", browserSync.reload);
+}
+
+// Tarefas principais
+const build = gulp.series(
+  gulp.parallel(sassTask, minifyCss, minifyJs, copyFiles)
+);
+const dev = gulp.series(build, gulp.parallel(browserSyncTask, watchFiles));
+
+// Exporta tarefas
+exports.sass = sassTask;
+exports.minifyCss = minifyCss;
+exports.minifyJs = minifyJs;
+exports.copy = copyFiles;
+exports.build = build;
+exports.dev = dev;
+exports.default = build;
